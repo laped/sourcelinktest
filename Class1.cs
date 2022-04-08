96f07453ebbe2e4c06f63bd28a92fd3feafa53e9
Lasse Pedersen <lp@uvdata.dk>
2022-04-08 10:04:57 +0200

    Init

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace SourceLinkTest;
+
+public static class Class1
+{
+    public static bool ParseSomething(string? text)
+    {
+        if (text == null) throw new ArgumentNullException(nameof(text));
+
+        var result = text.Equals("yes", StringComparison.OrdinalIgnoreCase) || text.Equals("ja", StringComparison.OrdinalIgnoreCase)
+            ? true : false;
+
+        return result;
+    }
+}
